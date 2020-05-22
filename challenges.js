58ccaba7a95212084809fa971c5e73b0fae3d639
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-05-21 23:40:08 -0400

    add eventListen to roll btn

--- challenges.js
+++ challenges.js
@@ -5,5 +5,14 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 ) + 1;
-console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    dice = Math.floor(Math.random() * 6 ) + 1;
+    console.log(dice);
+})
